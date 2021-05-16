5e01f7549ab7f7c8ec22eac33ea7c776b41645d1
oliveiraemerson <emerson.oliveira@systemstech.com.br>
2021-05-16 13:38:18 -0300

    - Instalando ASPNET Core Identity - Integrando ao Projeto - Razor Pages

---
+++ antes/Item02/CasaDoCodigo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using CasaDoCodigo.Areas.Identity.Data;
+using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CasaDoCodigo.Areas.Identity.IdentityHostingStartup))]
+namespace CasaDoCodigo.Areas.Identity
+{
+    //PRECISAMOS TRADUZIR AS MENSAGENS DE ERRO DE SENHAS:
+
+    //ORIGINAL:
+    //=========
+    //Passwords must have at least one non alphanumeric character.
+    //Passwords must have at least one lowercase('a' - 'z').
+    //Passwords must have at least one uppercase('A' - 'Z').
+
+    //PT-BR:
+    //=========
+    //A senha deve ter pelo menos um caractere alfanumérico.
+    //A senha deve ter pelo menos uma letra minúscula('a' - 'z').
+    //A senha deve ter pelo menos uma letra minúscula('a' - 'z').
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppIdentityContext>(options =>
+                    options.UseSqlite(
+                        context.Configuration.GetConnectionString("AppIdentityContextConnection")));
+
+                services.AddDefaultIdentity<AppIdentityUser>(option =>
+                    {
+                        option.Password.RequireNonAlphanumeric = 
+                        option.Password.RequireLowercase = 
+                        option.Password.RequireUppercase = false;
+                    })
+                    .AddErrorDescriber<IdentityErrorDescriberPtBr>()
+                    .AddEntityFrameworkStores<AppIdentityContext>();
+            });
+        }
+    }
+
+    internal class IdentityErrorDescriberPtBr : IdentityErrorDescriber
+    {
+        public override IdentityError DefaultError()
+        {
+            return base.DefaultError();
+        }
+
+        public override IdentityError ConcurrencyFailure()
+        {
+            return base.ConcurrencyFailure();
+        }
+
+        public override IdentityError PasswordMismatch()
+        {
+            return base.PasswordMismatch();
+        }
+
+        public override IdentityError InvalidToken()
+        {
+            return base.InvalidToken();
+        }
+
+        public override IdentityError RecoveryCodeRedemptionFailed()
+        {
+            return base.RecoveryCodeRedemptionFailed();
+        }
+
+        public override IdentityError LoginAlreadyAssociated()
+        {
+            return base.LoginAlreadyAssociated();
+        }
+
+        public override IdentityError InvalidUserName(string userName)
+        {
+            return base.InvalidUserName(userName);
+        }
+
+        public override IdentityError InvalidEmail(string email)
+        {
+            return base.InvalidEmail(email);
+        }
+
+        public override IdentityError DuplicateUserName(string userName)
+        {
+            return base.DuplicateUserName(userName);
+        }
+
+        public override IdentityError DuplicateEmail(string email)
+        {
+            return base.DuplicateEmail(email);
+        }
+
+        public override IdentityError InvalidRoleName(string role)
+        {
+            return base.InvalidRoleName(role);
+        }
+
+        public override IdentityError DuplicateRoleName(string role)
+        {
+            return base.DuplicateRoleName(role);
+        }
+
+        public override IdentityError UserAlreadyHasPassword()
+        {
+            return base.UserAlreadyHasPassword();
+        }
+
+        public override IdentityError UserLockoutNotEnabled()
+        {
+            return base.UserLockoutNotEnabled();
+        }
+
+        public override IdentityError UserAlreadyInRole(string role)
+        {
+            return base.UserAlreadyInRole(role);
+        }
+
+        public override IdentityError UserNotInRole(string role)
+        {
+            return base.UserNotInRole(role);
+        }
+
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return base.PasswordTooShort(length);
+        }
+
+        public override IdentityError PasswordRequiresUniqueChars(int uniqueChars)
+        {
+            return base.PasswordRequiresUniqueChars(uniqueChars);
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresNonAlphanumeric),
+                Description = "A senha deve ter pelo menos um caractere alfanumérico."
+            };
+        }
+
+        public override IdentityError PasswordRequiresDigit()
+        {
+            return base.PasswordRequiresDigit();
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresLower),
+                Description = "A senha deve ter pelo menos uma letra minúscula('a' - 'z')."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError
+            {
+                Code = nameof(PasswordRequiresUpper),
+                Description = "A senha deve ter pelo menos uma letra maiúscula('A' - 'Z')."
+            };
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
